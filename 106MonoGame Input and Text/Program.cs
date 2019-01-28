f89347ca2785df1737ace99ec15d433b646d1457
Bobby Levin <bil1616@ad.rit.edu>
2019-01-28 09:05:56 -0500

    Add project files.

---
+++ 106MonoGame Input and Text/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _106MonoGame_Input_and_Text
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
